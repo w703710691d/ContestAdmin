e0a2c3ab548113f9ec2481b078a706de9d23fb57
李洋阳 <w703710691d@163.com>
2020-10-06 19:11:11 +0800

    使用Druid连接数据库，更新mysql连接器

--- src/com/core/CoreConfig.java
+++ src/com/core/CoreConfig.java
@@ -1,49 +1,51 @@
 package com.core;
 
 
+import com.alibaba.druid.wall.WallFilter;
 import com.jfinal.config.*;
 import com.jfinal.core.JFinal;
 import com.jfinal.ext.handler.ContextPathHandler;
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
-import com.jfinal.plugin.c3p0.C3p0Plugin;
+import com.jfinal.plugin.druid.DruidPlugin;
 import com.main.MainController;
 
 import api.ApiController;
 import contest.ContestController;
 import com.main.admin.AdminController;
 
-public class CoreConfig extends JFinalConfig 
-{
-	public void configConstant(Constants me) 
-	{
-		me.setDevMode(true);
-	}
-	public void configRoute(Routes me) 
-	{
-		me.add("/", MainController.class);
-		me.add("/contest", ContestController.class);
-		me.add("/admin", AdminController.class);
-		me.add("/api", ApiController.class);
-	}
-	public void configPlugin(Plugins me)
-	{
-		loadPropertyFile( "db_config.txt");
-    	C3p0Plugin cp  =  new  C3p0Plugin(getProperty("url"), getProperty("user"), getProperty("password"));
-    	me.add(cp);
-    	ActiveRecordPlugin arp = new ActiveRecordPlugin(cp);
-    	me.add(arp);
-	}
-	public void configInterceptor(Interceptors me)
-	{
-		me.addGlobalActionInterceptor(new GlobalActionInterceptor());
-	}
-	@Override
-	public void configHandler(Handlers me)
-	{
-		me.add(new ContextPathHandler("ctx_path"));
-	}
-	public static void main(String[] args)
-	{
-		JFinal.start("WebRoot", 80, "/", 5);
-	}
+public class CoreConfig extends JFinalConfig {
+    public void configConstant(Constants me) {
+        me.setDevMode(true);
+    }
+
+    public void configRoute(Routes me) {
+        me.add("/", MainController.class);
+        me.add("/contest", ContestController.class);
+        me.add("/admin", AdminController.class);
+        me.add("/api", ApiController.class);
+    }
+
+    public void configPlugin(Plugins me) {
+        loadPropertyFile("db_config.txt");
+        DruidPlugin druidPlugin = new DruidPlugin(getProperty("url"), getProperty("user"), getProperty("password"));
+        WallFilter wall = new WallFilter();
+        wall.setDbType("mysql");
+        druidPlugin.addFilter(wall);
+        me.add(druidPlugin);
+        ActiveRecordPlugin arp = new ActiveRecordPlugin(druidPlugin);
+        me.add(arp);
+    }
+
+    public void configInterceptor(Interceptors me) {
+        me.addGlobalActionInterceptor(new GlobalActionInterceptor());
+    }
+
+    @Override
+    public void configHandler(Handlers me) {
+        me.add(new ContextPathHandler("ctx_path"));
+    }
+
+    public static void main(String[] args) {
+        JFinal.start("WebRoot", 80, "/", 5);
+    }
 }
